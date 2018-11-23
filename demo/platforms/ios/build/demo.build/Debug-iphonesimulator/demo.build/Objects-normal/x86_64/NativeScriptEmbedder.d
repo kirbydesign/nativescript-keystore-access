a14f6c0653e9d2cd85d2819e767e25597c401ef4
Thor Anker Kvisgård Lange <thor.lange@gmail.com>
2018-11-23 13:05:48 +0100

    Clean up readying for automated build

--- demo/platforms/ios/build/demo.build/Debug-iphonesimulator/demo.build/Objects-normal/x86_64/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/neigaard/Documents/JyskeBank/relationsbank/nativescript-keystore-access/demo/platforms/ios/demo/demo-Prefix.pch \
-  /Users/neigaard/Documents/JyskeBank/relationsbank/nativescript-keystore-access/demo/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/neigaard/Documents/JyskeBank/relationsbank/nativescript-keystore-access/demo/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
