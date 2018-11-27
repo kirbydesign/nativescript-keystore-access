9f5dbbe92ca0998de4a8a8d62cf387f992bce61f
Thor Anker Kvisgård Lange <lange@kvisgaard-lange.dk>
2018-11-27 08:31:26 +0100

    Feature/travis build (#1)
    Finally got the Travis CI pipeline right. Was an error in the default configuration and problems hitting the right Android simulator.

--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -3,10 +3,10 @@ var keystoreAccess = new KeystoreAccess();
 
 describe("greet function", function() {
     it("exists", function() {
-        expect(keystoreAccess.greet).toBeDefined();
+        expect(keystoreAccess.available).toBeDefined();
     });
 
     it("returns a string", function() {
-        expect(keystoreAccess.greet()).toEqual("Hello, NS");
+        expect(keystoreAccess.available()).toBeDefined();
     });
 });
